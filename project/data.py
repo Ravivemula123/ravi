bd543a69c791b21cb81399ae0d45e579b5a0a8f3
vemularavi_500apps <ravi1255.500apps@gmail.com>
2022-12-08 14:02:48 +0530

    fgsdfhsdhh
    Signed-off-by: vemularavi_500apps <ravi1255.500apps@gmail.com>

--- project/data.py
+++ project/data.py
@@ -1,1 +1,10 @@
-print ("hello")
+from flask import * 
+app =Flask(__name__)
+
+@app.route ('/')
+def data1():
+    num1=int(request.json['num1'])
+    num2=int(request.json['num2'])
+    return f'the sum of{num1} and {num2} is {num1+num2}'
+if __name__ == '__main__':
+    app.run(debug=True)
